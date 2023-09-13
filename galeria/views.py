9b728754e34a42fad2927a656d3e806cb51a1b54
Joao Oliveira <j.oliveiradg@gmail.com>
2023-09-12 22:06:31 -0300

    feat: criando static page 2

--- galeria/views.py
+++ galeria/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request) :
   return render(request, 'galeria/index.html')
 
+def imagem(request) :
+  return render(request, 'galeria/imagem.html')
+
+
+
--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('imagem/', imagem )
 ]
